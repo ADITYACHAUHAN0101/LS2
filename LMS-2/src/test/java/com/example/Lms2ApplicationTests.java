4cd085e34e209a5b5cf3431aea1ae63cbeef3048
BINYTECH <39939885+BINYTECH@users.noreply.github.com>
2022-12-21 21:35:13 +0530

    First Commit

---
+++ LMS-2/src/test/java/com/example/Lms2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lms2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
